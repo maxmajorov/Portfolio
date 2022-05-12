f54d7d60d663e296b25de8ac23ffe41cef76462f
Maxim Predko <maxpredko@gmail.com>
2022-05-12 08:41:11 +0300

    Updates

---
+++ precache-manifest.0ae0fb7081a880dcb92d27e09d9583c6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3c98a7357f9d7af4a90fb5cdcbd495",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "3f965dd2abee61f55a47",
+    "url": "/Portfolio/static/css/main.4b9b5d3a.chunk.css"
+  },
+  {
+    "revision": "b1e8bbb181a73176a6d1",
+    "url": "/Portfolio/static/js/2.864034d8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Portfolio/static/js/2.864034d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f965dd2abee61f55a47",
+    "url": "/Portfolio/static/js/main.90a9d93d.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "b24c88e4d5a35b6b455cdee870bc1373",
+    "url": "/Portfolio/static/media/avatar.b24c88e4.jpg"
+  },
+  {
+    "revision": "eac3c0fa9f5476797f3a23c0c483eaa3",
+    "url": "/Portfolio/static/media/devider.eac3c0fa.svg"
+  },
+  {
+    "revision": "65fe5ca450758865a59d82b01d637a6e",
+    "url": "/Portfolio/static/media/react.65fe5ca4.png"
+  },
+  {
+    "revision": "b8b5a3d12dd4f31837de856efe18fcfa",
+    "url": "/Portfolio/static/media/redux.b8b5a3d1.png"
+  },
+  {
+    "revision": "9b541fa316cc0c5a83c99da02e889a71",
+    "url": "/Portfolio/static/media/ts.9b541fa3.png"
+  }
+]);
