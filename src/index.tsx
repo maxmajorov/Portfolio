04c4708c2ceacb9e6cd88993a2f8ad137195bd53
Maxim Predko <maxpredko@gmail.com>
2022-06-21 19:18:27 +0300

    change index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-
-root.render(
+ReactDOM.render(
   <HashRouter>
     <App />
-  </HashRouter>
+  </HashRouter>,
+  document.getElementById("root")
 );
