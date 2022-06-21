dcf0d8b8b3157cc72420161040f12eac91d9acc7
Maxim Predko <maxpredko@gmail.com>
2022-06-21 18:16:14 +0300

    refactor contacts section

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,44 +1,73 @@
 import React from "react";
 import { Button } from "../../common/components/Button/Button";
 import { Title } from "../../common/components/Title/Title";
-import classes from "./Contacts.module.css";
+import classes from "./Contacts.module.scss";
 
 export const Contacts: React.FC = () => {
   return (
     <section id="contacts" className={classes.contactsBlock}>
-      <Title title="get in touch" />
-      <div className={classes.inputField}>
-        <form className={classes.contacts__form}>
-          <h3 className={classes.contactsForm__title}>
-            A quick way to discuss details
-          </h3>
-          <input
-            type="text"
-            name="name"
-            placeholder="Name"
-            className={classes.form__input}
-          />
-          <input
-            type="tel"
-            name="phone"
-            placeholder="Phone"
-            className={classes.form__input}
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            className={classes.form__input}
-          />
-          <textarea
-            name="message"
-            placeholder="Your message"
-            rows={20}
-            className={classes.form__input}
-          ></textarea>
-
-          <Button />
-        </form>
+      <div className={classes.container}>
+        <Title title="get in touch" />
+        <div className={classes.inputField}>
+          <div className={classes.col_8}>
+            <form className={classes.form}>
+              <div className={classes.formRow}>
+                <div className={classes.col_6}>
+                  <div className={classes.formGroup}>
+                    <input
+                      type="text"
+                      name="name"
+                      placeholder="Name"
+                      className={classes.formInput}
+                    />
+                  </div>
+                </div>
+                <div className={classes.col_6}>
+                  <div className={classes.formGroup}>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                      className={classes.formInput}
+                    />
+                  </div>
+                </div>
+                <div className={classes.col_6}>
+                  <div className={classes.formGroup}>
+                    <input
+                      type="tel"
+                      name="phone"
+                      placeholder="Phone"
+                      className={classes.formInput}
+                    />
+                  </div>
+                </div>
+                <div className={classes.col_6}>
+                  <div className={classes.formGroup}>
+                    <input
+                      type="text"
+                      name="subject"
+                      placeholder="Subject"
+                      className={classes.formInput}
+                    />
+                  </div>
+                </div>
+                <div className={classes.col_12}>
+                  <div className={classes.formGroup}>
+                    <textarea
+                      name="message"
+                      placeholder="Your message"
+                      className={classes.formInput}
+                    ></textarea>
+                  </div>
+                </div>
+                <div className={classes.col_12}>
+                  <Button />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </section>
   );
